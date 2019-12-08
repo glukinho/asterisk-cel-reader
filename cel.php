e08709fafe92b895d8912c97f0ed6299b5ae7936
glukinho <glukinho@gmail.com>
2019-12-08 22:29:11 +0300

    Update cel.php

--- cel.php
+++ cel.php
@@ -7,32 +7,14 @@ include 'classes/MPA_CEL_Call.php';
 include 'classes/safemysql.class.php';
 
 $db_options = array(
-	'user'    => 'root',
-	'pass'    => '',
+	'user'    => 'user',
+	'pass'    => 'pass',
 	'db'      => 'asteriskcdrdb',
-	'charset' => 'latin1'
+	'charset' => 'utf8'
 );
-
-// $linkedid = '1575633306.274662';
-// $linkedid = '1575719830.275368';
-// $linkedid = '1575644057.275354';
-
-
-
-// $linkedid = '1567403575.132993';
-
-// $call = new MPA_CEL_Call($linkedid, $db_options);
-// $call->load();
-// var_dump($call->getDirection());
-// var_dump($call->isAnswered());
-// var_dump($call->getStartTimestamp());
-// var_dump($call->isWorkTime());
-// die;
-
 
 
 $calls_arr = file('calls-sep.txt', FILE_IGNORE_NEW_LINES);
-// print_r ($calls_arr);
 
 $fields = [
 	'linkedid',
@@ -74,31 +56,3 @@ foreach ($calls_arr as $linkedid) {
 	
 	echo implode(',', $csv) . PHP_EOL;
 }
-
-
-
-
-
-// print_r($call->getEvents()->filter('eventtype', 'CHAN_START'));
-
-// print_r($call);
-// print_r($call->getEvents()->filter('eventtype', 'BRIDGE_ENTER')->last()->get('extra'));
-// $first_channel = $call->getEvents()->filter('eventtype', 'CHAN_START')->first()->get('channame');
-// echo $first_channel;
-// echo ( begins($first_channel, 'SIP/mastertel') ? 'incoming' : 'outgoing' );
-
-// var_dump($call->getDirection());
-// var_dump($call->isAnswered());
-// var_dump($call->getStartTimestamp());
-// var_dump($call->isWorkTime());
-
-
-
-
-
-
-
-
-
-
-
